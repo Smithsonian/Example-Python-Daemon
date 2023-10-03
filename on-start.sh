31f0cd02db97997a4d78292b0614bd64bfce6a3e
Paul Grimes <pgrimes@labrx.cfa.harvard.edu>
2023-10-03 11:02:03 -0500

    Switch to miniconda from anaconda

--- on-start.sh
+++ on-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source $HOME/anaconda3/etc/profile.d/conda.sh
+source $HOME/miniconda3/etc/profile.d/conda.sh
 conda activate smax # change to your conda environment's name
 # -u: unbuffered output
 python -u $HOME/.config/systemd/example-smax-daemon/example-smax-daemon.py
