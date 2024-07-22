3fc4f8a84c1ce01148ce003792738287ef6a4ae9
Paul Grimes <pgrimes@cfa.harvard.edu>
2024-07-22 11:39:05 -0400

    Simplify on-start to use simple call to python in specified env

--- src/smax_daemon/on_start.sh
+++ src/smax_daemon/on_start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# >>> mamba initialize >>>
-export MAMBA_EXE='/opt/mamba/bin/mamba';
-export MAMBA_ROOT_PREFIX='/opt/mamba';
+CONDA_PREFIX='/opt/mamba';
+CONDA_ENV='example_smax_daemon';
 
-# -u: unbuffered output
-$MAMBA_EXE activate example_smax_daemon
-exec "/usr/local/lib/example_smax_daemon/example_smax_daemon.py"
+eval "$CONDA_PREFIX/envs/$CONDA_ENV/bin/python example_smax_daemon.py"
